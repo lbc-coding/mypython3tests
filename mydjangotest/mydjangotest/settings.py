3a5acfbc6fe49b17dfeebd81383e2d5aa4328115
liubaochang <543730416@qq.com>
2020-04-28 00:03:21 +0800

    静态资源及路由设置\媒体资源及路由设置\模板路由设置

--- mydjangotest/mydjangotest/settings.py
+++ mydjangotest/mydjangotest/settings.py
@@ -56,7 +56,8 @@ ROOT_URLCONF = 'mydjangotest.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), 
+                        os.path.join(BASE_DIR,'index/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,4 +120,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/Allstatic/'
+# 添加所有静态资源路径
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), os.path.join(BASE_DIR, 'index/Mystatic'), ]
+# 设置服务器静态资源映射
+STATIC_ROOT = os.path.join(BASE_DIR, 'Allstatic')
+
+# 添加媒体资源路由信息
+MEDIA_URL = '/media/'
+# 获取media完整路径信息
+MEDIA_URL = os.path.join(BASE_DIR, 'media/')
--- mydjangotest/mydjangotest/urls.py
+++ mydjangotest/mydjangotest/urls.py
@@ -14,11 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from index.views import index
-
+# 配置媒体文件夹media
+from django.views.static import serve
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index)
-]
+    path('', index),
+    re_path('media/(?P<path>.*)', serve, 
+    {'document_root': settings.MEDIA_ROOT}, name = 'media'), 
+    ]
